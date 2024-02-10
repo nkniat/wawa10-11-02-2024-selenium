d94b079fe5c459c8f3bae38aa8d3ae264941dc03
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-02-10 13:27:04 +0100

    logowanie do FB

--- cw3_fb.py
+++ cw3_fb.py
@@ -4,12 +4,34 @@ import time  # do sleep'a
 
 # inicjalizacja przeglądarki
 driver = webdriver.Chrome()
+driver.maximize_window()
 
 # Implicity wait - działa na wszystkie elementy na stronie
-# driver.implicitly_wait(10)
+driver.implicitly_wait(10)
 
 # otwarcie okna przeglądarki
 driver.get('https://www.facebook.com/')
+
+# zaakceptuj zgody
+accept = driver.find_element("xpath", "/html/body/div[3]/div[2]/div/div/div/div/div[4]/button[2]")
+accept.click()
+
+#logowanie
+user = "natas.knt@gmail.com"
+
+with open('dane.txt', 'r') as myfile:
+    password = myfile.read().replace('\n', '')
+
+# uzupełnienie danymi
+loginInput = driver.find_element("id", "email")
+loginInput.send_keys(user)
+
+passInput = driver.find_element("id", "pass")
+passInput.send_keys(password)
+
+# klikniecie login
+logMyIn = driver.find_element("name", "login")
+logMyIn.click()
 
 # zatrzymaj się na chwilkę
 # sleep może być użyty wielokrotnie, zatrzymuje skrypt w danym momencie na określoną ilość czasu
